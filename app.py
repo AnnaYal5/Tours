7ea593619b5e3192285a801224e30a6749eea341
Elaronnd <elaronnd@gmail.com>
2024-11-23 20:04:02 +0200

    lesson

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import data
 
 app = Flask(__name__)
@@ -6,12 +6,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", title=data.title,departures=data.departures,tours=data.tours)
+    return render_template("index.html", title=data.title, departures=data.departures, tours=data.tours)
 
 
-@app.route("/departures")
-def departures():
-    return render_template("departures.html")
+@app.route("/departures/<departure>")
+def departures(departure: str):
+    tours_departure = []
+    tours_list = []
+    for key, tour in data.tours.items():
+        if tour.get("departure") == departure.lower():
+            tours_departure.append(key)
+    for tour_departure in tours_departure:
+        tours_list.append(data.tours.get(tour_departure))
+    print(tours_list)
+    return render_template("departures.html", tours_departure=tours_list, tours=data.tours)
 
 
 @app.route("/tours")
